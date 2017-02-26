2f24f1816f35929efbf434608b832d4f54514f52
tsurumeso <nstm101339@gmail.com>
2017-02-26 19:12:01 +0900

    Modify for if-statement

--- filter.py
+++ filter.py
@@ -16,7 +16,7 @@ def bilateral_filter(src, diameter=5, sigma_space=20, sigma_color=20):
             dst = tmp.copy()
             dst[:, :, 1] = _filter._l_bilateral_solver(
                 tmp[:, :, 1], diameter, sigma_space, sigma_color)
-        if src.mode == 'YCbCr':
+        elif src.mode == 'YCbCr':
             dst = tmp.copy()
             dst[:, :, 0] = _filter._l_bilateral_solver(
                 tmp[:, :, 0], diameter, sigma_space, sigma_color)
